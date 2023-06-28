40440c9c383972a516d67d0081c5b231be237c50
yeisonS <yeisonsilgadocalao200224@gmail.com>
2023-06-28 02:49:44 -0500

    Se agrega el metodo para recuperar contraseña

--- src/controlador/Vista1Controller.java
+++ src/controlador/Vista1Controller.java
@@ -1,10 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-package controlador;
+ */package controlador;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
@@ -18,16 +18,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 
-/**
- * FXML Controller class
- *
- * @author Usuario
- */
 public class Vista1Controller implements Initializable {
 
     @FXML
@@ -41,45 +36,15 @@ public class Vista1Controller implements Initializable {
     @FXML
     private Button Bolvidar;
 
-    public void show() {
-        stage.show();
-    }
-
-    
     private Stage stage;
-   
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
-    }    
-    
-    
-     
-   
-    public void setStage(Stage primaryStage) {
-        stage = primaryStage;
-       
     }
 
- 
-
-    @FXML
-    private void click1(ActionEvent event) {
-         try{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/RegistroVisual2.fxml"));
-        Parent root = loader.load();
-        RegistroVisual2Controller controller = loader.getController();
-        
-        
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        controller.init(stage,this );
-        stage.show();
-        this.stage.close();
-    }catch (IOException e){System.out.println(e.getMessage());
-     }
+    public void setStage(Stage primaryStage) {
+        stage = primaryStage;
     }
 
     @FXML
@@ -89,97 +54,149 @@ public class Vista1Controller implements Initializable {
     @FXML
     private void contrasena(ActionEvent event) {
     }
+
+    public class MyController {
+    @FXML
+   
+
+    public void initialize() {
+        Bolvidar.getStyleClass().add("hovered-button");
+        Bolvidar.setOnMouseEntered(event -> Bolvidar.getStyleClass().add("hovered-button"));
+        Bolvidar.setOnMouseExited(event -> Bolvidar.getStyleClass().remove("hovered-button"));
+    }
+}
+    
+    @FXML
+    private void click1(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/RegistroVisual2.fxml"));
+            Parent root = loader.load();
+            RegistroVisual2Controller controller = loader.getController();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            controller.init(stage, this);
+            stage.show();
+            this.stage.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     @FXML
     private void clickR(ActionEvent event) {
-         try{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/Recuperar.fxml"));
-        Parent root = loader.load();
-        RecuperarController controller = loader.getController();
-        
-        
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        controller.initu(stage,this );
-        stage.show();
-      
-    }catch (IOException e){System.out.println(e.getMessage());
-     }
-    }
-private void mostrarMensajeError(String titulo, String mensaje) {
-    Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle(titulo);
-    alert.setHeaderText(null);
-    alert.setContentText(mensaje);
-    alert.showAndWait();
-}
-    
-     @FXML
-     
-        
-    public void click(ActionEvent event) {
-    
-     String Correo = usuariollenar.getText().trim();
-    String Contraseña = contrasenallenar.getText().trim();
+        String telefonoRecuperar = JOptionPane.showInputDialog(null, "Por favor, ingrese su número de teléfono:");
 
-    System.out.println("Correo: " + Correo);
-    System.out.println("Contraseña: " + Contraseña);
-
-    // Validar que los campos no estén vacíos
-    if (Correo.isEmpty() || Contraseña.isEmpty()) {
-        mostrarMensajeError("Error", "Por favor, ingrese el teléfono o correo y la contraseña.");
-        return;
+        // Validar el número de teléfono
+        if (validarTelefonoRecuperar(telefonoRecuperar)) {
+            mostrarMensajeInformacion("Recuperación de contraseña", "Un asesor se comunicará lo antes posible.");
+        } else {
+            mostrarMensajeError("Error", "El número de teléfono no coincide con nuestros registros.");
+        }
     }
 
-    // Verificar si los datos de inicio de sesión son válidos
-    if (verificarCredenciales(Correo, Contraseña)) {
-        mostrarMensajeError("Error", "Los datos de inicio de sesión son incorrectos.");
-        return;
+    private void mostrarMensajeInformacion(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 
-    // Abrir la siguiente ventana
-    abrirVentanaExistente("/vista/InicioVista1.fxml");
+    private boolean validarTelefonoRecuperar(String telefonoRecuperar) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("usuarios.txt"));
+            String line;
 
-    // Cerrar la ventana actual
-    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    stage.close();
-}
-
-private boolean verificarCredenciales(String Correo, String Contraseña) {
-    try {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("usuarios.txt"));
-        String line;
-
-        while ((line = bufferedReader.readLine()) != null) {
-            // Verificar si la línea comienza con el correo electrónico y la contraseña
-            System.out.println("Comparando línea: " + line);
-            if (line.startsWith("Correo: " + Correo) && line.startsWith("Contraseña: " + Contraseña)) {
-                bufferedReader.close();
-                return true; // Credenciales válidas
+            while ((line = bufferedReader.readLine()) != null) {
+                // Verificar si la línea contiene el número de teléfono
+                if (line.contains("Teléfono: " + telefonoRecuperar)) {
+                    bufferedReader.close();
+                    return true; // Número de teléfono válido
+                }
             }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        bufferedReader.close();
-    } catch (IOException e) {
-        e.printStackTrace();
+        return false; // Número de teléfono inválido
     }
 
-    return false; // Credenciales inválidas
+    private void mostrarMensajeError(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void click(ActionEvent event) {
+        String cCorreo = usuariollenar.getText().trim();
+        String cContraseña = contrasenallenar.getText().trim();
+
+        System.out.println("Correo: " + cCorreo);
+        System.out.println("Contraseña: " + cContraseña);
+
+        File file = new File("usuarios.txt");
+        System.out.println(file.getAbsolutePath());
+
+        // Validar que los campos no estén vacíos
+        if (cCorreo.isEmpty() || cContraseña.isEmpty()) {
+            mostrarMensajeError("Error", "Por favor, ingrese el teléfono o correo y la contraseña.");
+            return;
+        }
+
+        // Verificar si los datos de inicio de sesión son válidos
+        if (!verificarCredenciales(cCorreo, cContraseña)) {
+            mostrarMensajeError("Error", "Los datos de inicio de sesión son incorrectos.");
+            return;
+        }
+
+        // Abrir la siguiente ventana
+        abrirVentanaExistente("/vista/InicioVista1.fxml");
+
+        // Cerrar la ventana actual
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    private boolean verificarCredenciales(String cCorreo, String cContraseña) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("usuarios.txt"));
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                // Verificar si la línea comienza con el correo electrónico y la contraseña
+                System.out.println("Comparando línea: " + line);
+                if (line.contains("Correo: " + cCorreo) && line.contains("Contraseña: " + cContraseña)) {
+                    bufferedReader.close();
+                    return true; // Credenciales válidas
+                }
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false; // Credenciales inválidas
+    }
+
+    private void abrirVentanaExistente(String rutaFXML) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(rutaFXML));
+            Parent root = fxmlLoader.load();
+            Stage nuevaVentana = new Stage();
+            nuevaVentana.setScene(new Scene(root));
+            nuevaVentana.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+   
 }
-
-
-private void abrirVentanaExistente(String rutaFXML) {
-    try {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(rutaFXML));
-        Parent root = fxmlLoader.load();
-        Stage nuevaVentana = new Stage();
-        nuevaVentana.setScene(new Scene(root));
-        nuevaVentana.show();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    }
-}
-        
-    
-
